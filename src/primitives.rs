5edb8aa85818c157af0a7f8bf7d301e6aa742e84
berciuliviu <lberciu@amazon.com>
2021-03-22 14:04:24 +0200

    Fix cargo clippy error
    Fix cargo clippy reversed_empty_ranges error.
    
    Signed-off-by: berciuliviu <lberciu@amazon.com>

--- src/primitives.rs
+++ src/primitives.rs
@@ -166,11 +166,12 @@ macro_rules! impl_versionize_arrays {
     }
 }
 
+#[allow(clippy::reversed_empty_ranges)]
 impl_versionize_arrays! {
-    0  1  2  3  4  5  6  7  8  9
-   10 11 12 13 14 15 16 17 18 19
-   20 21 22 23 24 25 26 27 28 29
-   30 31 32
+    1  2  3  4  5  6  7  8  9 10
+   11 12 13 14 15 16 17 18 19 20
+   21 22 23 24 25 26 27 28 29 30
+   31 32
 }
 
 impl<T> Versionize for Box<T>
